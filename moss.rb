e41d585aefb1a1fb8ff92fb1049132ebb6ec866d
ebookstein <ebookstein@newrelic.com>
2015-05-07 23:27:06 -0700

    Add @name attribute to Moss, change method of adding itself to the ecosystem by using the ecosystem API

--- moss.rb
+++ moss.rb
@@ -1,11 +1,12 @@
 class Moss
-  attr_reader :volume # only read, not set -- later, use attr_accessor
+  attr_reader :volume, :name # only read, not set -- later, use attr_accessor
 
   def initialize(ecosystem)
     # all that initialize should do is set attributes!!
     @volume = 1
-    @ecosystem = ecosystem # pass ecosystem as array for now
-    @ecosystem << self # automatically adds itself
+    @name = "moss"
+    @ecosystem = ecosystem
+    @ecosystem.add_animal(self) # automatically adds itself
   end
 
   def eat
