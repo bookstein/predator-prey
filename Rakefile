0bf067b435845126d0c130bdb31bbf084560a38c
ebookstein <ebookstein@newrelic.com>
2015-05-08 08:27:53 -0700

    Pull out commonalities between animals into Animal class. Move eat and reproduce functions into Animal entirely.

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 task :console do
   # order only matters if there are dependencies
   require_relative "ecosystem"
+  require_relative "animal"
   require_relative "moss" # --> "require" brings things into global Ruby namespace
   require_relative "hare"
   require_relative "lynx"
---
+++ animal.rb
@@ -0,0 +1,22 @@
+class Animal
+
+  attr_accessor :volume
+  attr_reader :adult_size, :name, :growth
+
+  def initialize(ecosystem)
+    @ecosystem = ecosystem
+    @ecosystem.add_animal(self) # automatically adds itself
+  end
+
+  def eat
+    @volume += @growth
+  end
+
+  def reproduce
+    if @volume > @adult_size
+      baby = self.class.new(@ecosystem)
+    end
+
+    baby
+  end
+end
