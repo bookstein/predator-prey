576096867aa5114cdb48a86436d2211273f8997b
ebookstein <ebookstein@newrelic.com>
2015-05-10 19:22:51 -0700

    Add test that reproduction causes animals to be added to ecosystem

--- test/ecosystem_test.rb
+++ test/ecosystem_test.rb
@@ -13,12 +13,35 @@ class EcosystemTest < Minitest::Test
     assert_equal 5, e.animals[:hare].length
     assert_equal 2, e.animals[:lynx].length
   end
-# create new animals
 
-# see that animals reproduce
+  # create new animals
+  def test_create_new_animals
+    e = ::Ecosystem.new(10, 5, 2)
+    m = ::Moss.new(e)
+    h = ::Hare.new(e)
+    l = ::Lynx.new(e)
+  end
 
 # create animals by reproduction, make sure they are added to ecosystem
+  def test_new_animals_added_to_ecosystem
+    e = Ecosystem.new(0,0,0)
+    m = Moss.new(e)
+    h = Hare.new(e)
+    l = Lynx.new(e)
 
-# create animals, make sure they grow the right amount when they eat
+    # grow animals to adult size (able to reproduce)
+    m.volume = m.adult_size
+    h.volume = h.adult_size
+    l.volume = l.adult_size
 
+    # reproduce
+    m.reproduce
+    h.reproduce
+    l.reproduce
+
+    # check added to ecosystem
+    assert_equal 1, e.animals[:moss].length
+    assert_equal 1, e.animals[:hare].length
+    assert_equal 1, e.animals[:lynx].length
+  end
 end
